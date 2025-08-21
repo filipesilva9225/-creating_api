1836b323b737b1d7b324ade2921a16b2983ca4be
filipesilva9225 <filipesilva9225@gmail.com>
2025-08-21 15:17:24 -0300

    add all librarys for the project

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import express, { request } from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/users", (req, res) => {});
+const users = [];
+
+app.post("/users", (req, res) => {
+  users.push(req.body);
+
+  res.status(201).json(req.body);
+});
+
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
 
 app.listen(3000);
