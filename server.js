cd0e7e32efa3f5ee231d1920ee512ba2ca8c619b
filipesilva9225 <filipesilva9225@gmail.com>
2025-08-21 15:53:20 -0300

    add all files

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
-import express, { request } from "express";
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "./generated/prisma/index.js";
 
+const prisma = new PrismaClient();
 const app = express();
+
 app.use(express.json());
 
-const users = [];
+// Criar usuário
+app.post("/users", async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+        age: req.body.age,
+      },
+    });
 
-app.post("/users", (req, res) => {
-  users.push(req.body);
-
-  res.status(201).json(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
-app.get("/users", (req, res) => {
-  res.status(200).json(users);
+// Listar usuários
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("🚀 Servidor rodando em http://localhost:3000");
+});
